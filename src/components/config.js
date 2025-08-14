50e0c29062ceaf4e5f31307849c315b82621a844
anishkumar68 <anishkumar231999@gmail.com>
2025-08-14 17:06:47 +0530

    updated config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,5 @@
 // src/components/config.js
 
-export const API_BASE_URL = "https://campusbot-back-end.onrender.com";
+// export const API_BASE_URL = "https://campusbot-back-end.onrender.com";
 // export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "https://campusbot-back-end-bo5u.onrender.com";
